cb2a7db58fc21232ba0d371a694d787497c615a7
Alexandr Gorban <ukrgorban@gmail.com>
2020-09-07 20:39:05 +0300

    Finish table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -31,12 +31,34 @@ class Dom {
     return this
   }
 
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
   on(eventType, callback) {
     this.$el.addEventListener(eventType, callback)
   }
 
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => {
+      this.$el.style[key] = styles[key]
+    })
   }
 }
 
